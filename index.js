ccbc93bd562e9b9233adf326868a4fdee3324782
tim neutkens <tim@weprovide.com>
2016-02-12 19:18:31 +0100

    Added comments and clearer error handling

--- index.js
+++ index.js
@@ -2,50 +2,69 @@
 var Promise = Promise || require("bluebird");
 const request = require('superagent-promise')(require('superagent'), Promise);
 const prefix = require('superagent-prefix');
+const suffix = require('superagent-suffix');
+const directorySize = require('directory-size');
 const fs = require('fs');
 
-const directorySize = require('directory-size');
-
-
+/**
+ * Create a get request to seoshop
+ * @param path
+ * @returns {Request}
+ */
 function createGetRequest(path) {
     return request
-        .get(path)
-        .use(prefix('http://api.webshopapp.com/nl'))
+        .get('/' + path)
+        .use(prefix('https://api.webshopapp.com/nl'))
+        .use(suffix('.json'))
         .auth(process.env.SEOSHOAPIKEY, process.env.SEOSHOPAPISECRET)
-        .set('Accept', 'application/json')
+        .set('Accept', 'application/json');
 }
 
-function catchErrors(err) {
-    console.log('Something went wrong: ', err.message);
+/**
+ * Catch promise errors / rejects
+ * @param errorName
+ * @returns {Function}
+ */
+function catchErrors(errorName) {
+    return function(error) {
+        console.log('Something went wrong with ' + errorName + ': ', error.message);
+    }
 }
 
-createGetRequest('/variants.json')
+// Request variants
+createGetRequest('variants')
     .query({ limit: 250 })
-    .query({ page: 4 })
+    .query({ page: 1 })
     .then(function parseVariants(result) {
         return result.body.variants.map(function createVariantObject(element) {
             return {
                 productId: element.product.resource.id,
-                variantEan: element.ean,
-                variantSku: element.sku
+                variantEan: element.ean
             }
         })
     })
     .then(function imageImporter(variants) {
+        // Loop through the parsed response
         variants.forEach(function loopVariants(variant) {
-            const directory = process.env.OUTPUTDIRECTORY + '/' + variant.productId + '/';
+            // Create directory for product
             new Promise(
                 function createDirectory(resolve) {
+                    // Create directory path from env variable
+                    const directory = process.env.OUTPUTDIRECTORY + '/' + variant.productId + '/';
+
+                    // Create directory if it doesn't exist
                     fs.mkdir(directory, function lookupDirectorySize() {
+                        // Check if directory is empty. If it is go further.
                         directorySize(directory).then(function validateDirectorySize(size) {
                             if(size === 0) {
-                                resolve();
+                                resolve(directory);
                             }
                         });
                     })
                 }
-            ).then(function() {
-                createGetRequest('/products/' + variant.productId + '/images.json')
+            ).then(function(directory) {
+                // Request images for product
+                createGetRequest('products/' + variant.productId + '/images')
                     .query({ limit: 250 })
                     .then(function parseRequest(res) {
                         return res.body.productImages.map(function createImageObject(element) {
@@ -53,12 +72,17 @@ createGetRequest('/variants.json')
                         })
                     })
                     .then(function getImages(images) {
+                        // Loop through images and request them
                         images.forEach(function loopThroughImages(image, index) {
+                            // Create filename
                             const filename = variant.variantEan + '-' + index + '.jpg';
+
+                            // Create path
                             const path = directory + filename;
+                            // Check if doesn't  already exist and it's size is not 0 bytes
                             new Promise(
                                 function checkFile(resolve, reject) {
-                                    fs.stat(path + filename, function checkFileStats(err, stats) {
+                                    fs.stat(path, function checkFileStats(err, stats) {
                                         if(err || stats.size === 0) {
                                             resolve();
                                         } else {
@@ -67,16 +91,16 @@ createGetRequest('/variants.json')
                                     });
                                 }
                             ).then(function() {
-                                request.get(image).then(function(res) {
+                                // Get image and write it to file
+                                request.get(image).then(function writeImage(res) {
                                     const imageFileStream = fs.createWriteStream(path);
                                     imageFileStream.end(res.body, function() {
                                         console.log('Written: ' + variant.productId)
                                     })
-                                }).catch(catchErrors);
-                            }).catch(catchErrors);
-
+                                }).catch(catchErrors('images'));
+                            }).catch(catchErrors('folder'));
                         })
-                    }).catch(catchErrors);
+                    }).catch(catchErrors('products'));
             });
         });
-    }).catch(catchErrors);
+    }).catch(catchErrors('variants'));
